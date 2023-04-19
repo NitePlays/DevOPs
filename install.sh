9660283c786b7cdc81fb5469ff3eb139cfc07915
NitePlays <126669908+NitePlays@users.noreply.github.com>
2023-04-19 13:54:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ export DEBIAN_FRONTEND=noninteractive
 # Install Jenkins
 sudo apt-get update
 sudo apt-get install -y openjdk-11-jdk
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y jenkins
 sudo systemctl start jenkins
